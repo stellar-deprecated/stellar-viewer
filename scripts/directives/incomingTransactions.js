948330aa708934b07631c534c471ebf3c5ee0a18
Jared Deckard <jared.deckard@gmail.com>
2014-08-20 16:23:39 -0700

    Revert "Issue 4"

--- scripts/directives/incomingTransactions.js
+++
@@ -1,7 +0,0 @@
-stellarExplorer.directive('incomingTransactions', function() {
-  return {
-    restrict: 'E',
-    scope: true,
-    templateUrl: './templates/incoming-transactions.html'
-  };
-});
